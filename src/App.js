8c780ab5c104e1a7d5301bc8bd583885ce2e67c7
Soham Datta <sohamdatta34@gmail.com>
2023-01-06 22:33:15 +0530

    created state for data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
+import { useState } from 'react';
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
+const DUMMY_DATA = [
+  { id: 'e1', title: 'Mouse', amount: 56, date: new Date(2023, 0, 1) },
+  { id: 'e2', title: 'Keyboard', amount: 26, date: new Date(2022, 4, 21) },
+  { id: 'e3', title: 'Monitor', amount: 45, date: new Date(2020, 1, 12) },
+  { id: 'e4', title: 'Mousepad', amount: 12, date: new Date(2021, 6, 11) },
+  { id: 'e5', title: 'Chair', amount: 12, date: new Date(2022, 5, 7) },
+];
 function App() {
-  const expenses = [
-    { id: 'e1', title: 'Mouse', amount: 56, date: new Date(2023, 0, 1) },
-    { id: 'e2', title: 'Keyboard', amount: 26, date: new Date(2022, 4, 21) },
-    { id: 'e3', title: 'Monitor', amount: 45, date: new Date(2020, 1, 12) },
-    { id: 'e4', title: 'Mousepad', amount: 12, date: new Date(2021, 6, 11) },
-    { id: 'e5', title: 'Chair', amount: 12, date: new Date(2022, 5, 7) },
-  ];
+  const [data, setData] = useState(DUMMY_DATA)
 
   function addExpense(expense) {
-    console.log(expense)
+    // setData()
   }
 
   return (
     <div>
       <NewExpense onAddExpense={addExpense} />
-      <Expenses items={expenses} />
+      <Expenses items={DUMMY_DATA} />
     </div >
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,19 @@
+import { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 import Card from '../UI/Card';
 
 function Expenses(props) {
+
+    const [year, setYear] = useState('2020')
     const filterChange = (selectedYear) => {
-        console.log(selectedYear)
+        setYear(selectedYear)
     }
     return (
         <div>
             <Card className='expenses'>
-                <ExpensesFilter onFilterChange={filterChange} />
+                <ExpensesFilter selectedYear={year} onFilterChange={filterChange} />
                 {props.items.map((expense) => (
                     <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
                 ))}
